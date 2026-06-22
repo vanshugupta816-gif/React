8653c63511fcf6460aab73b30ee45d5bf61ab843
Vanshika Gupta <vanshugupta816@gmail.com>
2026-06-22 13:54:48 +0530

    vite react Project

---
+++ viteReact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai.jsx"
+
+function App() {
+  const username = "Vanshu is goodgurll"
+  return (
+    // is method ko hum kehte h fragment return krdo
+    <>   
+      <Chai/> 
+      <h1>hello cutiee with vite | vanshika {username}</h1> 
+      <p>test para</p>
+    </> 
+  )
+}
+export default App
---
+++ viteReact/src/main.jsx
@@ -0,0 +1,41 @@
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https:/google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit google'
+// }
+
+const anotherElement = (  
+  <a href="https://github.com/vanshugupta816-gif" target= '_blank'>Visit google</a>
+)
+
+const anotherUser = " chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotherUser
+)
+
+createRoot(document.getElementById('root')).render(
+  
+    //<App />
+    //anotherElement
+    reactElement
+  
+)
